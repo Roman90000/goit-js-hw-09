39f5d67fe3a39a2ab45a9d839291a45d1d5891e7
Roman90000 <95108009+Roman90000@users.noreply.github.com>
2023-07-11 13:57:51 +0000

    Deploying to gh-pages from @ Roman90000/goit-js-hw-09@7431f1b54b3d353c6f7646e6627c368b33fe0409 🚀

---
+++ 01-color-switcher.5077e394.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){id=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),t.disabled=!0,console.log(t)}),1e3)})),e.addEventListener("click",(function(){clearInterval(id),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.5077e394.js.map
---
+++ 01-color-switcher.82709f91.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){id=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,t.disabled=!0,console.log(t)}),1e3)})),e.addEventListener("click",(function(){clearInterval(id),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.82709f91.js.map
