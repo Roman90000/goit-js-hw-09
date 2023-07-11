b3b38b31c873aafe97890557c4a2e7a68ab17a5b
Roman90000 <95108009+Roman90000@users.noreply.github.com>
2023-07-11 14:33:03 +0000

    Deploying to gh-pages from @ Roman90000/goit-js-hw-09@24db9e4781eee1f612da5064ccb436441bd998cb 🚀

---
+++ 01-color-switcher.e30ea583.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){id=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,t.disabled=!0,e.disabled=!1}),1e3)})),e.addEventListener("click",(function(){clearInterval(id),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.e30ea583.js.map
